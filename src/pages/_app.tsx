450c7bf6ed29b8e6f92ffeb2e34af3ed5261288a
Peter Abah <peterabah.ob@gmail.com>
2023-04-10 17:13:22 +0100

    Add protected routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,41 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
+import type { NextPage } from "next";
+import { SessionProvider, useSession } from "next-auth/react";
+import { ReactElement, ReactNode } from "react";
+
+export type NextPageWithAuth<P = {}, IP = P> = NextPage<P, IP> & {
+  auth?: boolean
+}
+
+type AppPropsWithAuth = AppProps & {
+  Component: NextPageWithAuth
+}
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: AppPropsWithAuth) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {Component.auth ? (
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </SessionProvider>
   );
+}
+
+function Auth({ children }: { children: ReactElement }) {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({ required: true });
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 }

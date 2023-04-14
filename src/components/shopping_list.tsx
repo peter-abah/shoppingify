808186d44580764d447f94be5fc5575bce0f0ce5
Peter Abah <peterabah.ob@gmail.com>
2023-04-15 00:42:52 +0100

    Add UI for when shopping list is empty

--- src/components/shopping_list.tsx
+++ src/components/shopping_list.tsx
@@ -13,7 +13,7 @@ export default function ShoppingList({ shoppingList }: Props) {
   const itemsByCategory = groupItemsByCategory(items);
 
   return (
-    <div className="bg-[#FFF0DE] px-12 h-screen overflow-y-auto grow shrink-0 max-w-sm fixed top-0 right-0">
+    <div className="bg-[#FFF0DE] px-10 h-screen overflow-y-auto grow shrink-0 w-[24rem] fixed top-0 right-0 flex flex-col">
       <div className="py-4 px-7 bg-[#80485B] my-11 text-white rounded-xl">
         <p className="font-bold max-w-[10rem] mb-3.5">
           Didn’t find what you need?
@@ -22,25 +22,36 @@ export default function ShoppingList({ shoppingList }: Props) {
           Add Item
         </button>
       </div>
-      <h2 className="text-2xl font-bold mb-10 flex items-center">
-        {name} <MdEdit className="text-lg ml-auto" />
-      </h2>
 
-      {Array.from(itemsByCategory.keys()).map((category) => (
-        <div className="mb-12">
-          <h3 className="text-sm text-[#828282] mb-6 font-medium">
-            {category}
-          </h3>
-          <ol>
-            {itemsByCategory.get(category)!.map((item) => (
-              <li className="flex justify-between items-center mb-6 gap-2">
-                <span className="text-lg font-medium">{item.name}</span>{" "}
-                <button className="w-16 h-8 grid place-items-center text-[#F9A10A] border-2 border-[#F9A10A] rounded-3xl text-xs">{item.count} pcs</button>
-              </li>
-            ))}
-          </ol>
+      {items.length > 0 ? (
+        <>
+          <h2 className="text-2xl font-bold mb-10 flex items-center">
+            {name} <MdEdit className="text-lg ml-auto" />
+          </h2>
+
+          {Array.from(itemsByCategory.keys()).map((category) => (
+            <div className="mb-12">
+              <h3 className="text-sm text-[#828282] mb-6 font-medium">
+                {category}
+              </h3>
+              <ol>
+                {itemsByCategory.get(category)!.map((item) => (
+                  <li className="flex justify-between items-center mb-6 gap-2">
+                    <span className="text-lg font-medium">{item.name}</span>{" "}
+                    <button className="w-16 h-8 grid place-items-center text-[#F9A10A] border-2 border-[#F9A10A] rounded-3xl text-xs">
+                      {item.count} pcs
+                    </button>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          ))}
+        </>
+      ) : (
+        <div className="grow grid place-items-center bg-[url('/shopping_cart.svg')] bg-no-repeat bg-bottom">
+          <p className="w-fit text-xl font-bold">No items</p>
         </div>
-      ))}
+      )}
     </div>
   );
 }

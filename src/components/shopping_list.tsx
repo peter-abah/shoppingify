e44124422a0626804110d227b56b17cf72cf2446
Peter Abah <peterabah.ob@gmail.com>
2023-05-09 02:00:33 +0100

    Add name form to edit shopping list name

--- src/components/shopping_list.tsx
+++ src/components/shopping_list.tsx
@@ -1,11 +1,13 @@
+import React from "react";
+import { useForm } from "react-hook-form";
 import type { ItemInShoppingList } from "@prisma/client";
 import { useStore } from "zustand";
 import { useStoreContext } from "@/lib/store_context";
+import clsx from "clsx";
 import { MdEdit } from "react-icons/md";
 import Spinner from "./spinner";
 import { ActiveSideBar, ShoppingListUIState } from "@/lib/store";
 import ItemInList from "./item_in_list";
-import { useState } from "react";
 
 export default function ShoppingList() {
   const storeApi = useStoreContext();
@@ -30,7 +32,7 @@ export default function ShoppingList() {
   };
 
   return (
-    <div className="bg-[#FFF0DE] px-10 h-screen overflow-y-auto grow shrink-0 w-[24rem] fixed top-0 right-0 z-10 flex flex-col">
+    <div className="bg-[#FFF0DE] px-10 pb-36 h-screen overflow-y-auto grow shrink-0 w-[24rem] fixed top-0 right-0 z-10 flex flex-col">
       <div className="py-4 px-7 bg-[#80485B] my-11 text-white rounded-xl">
         <p className="font-bold max-w-[10rem] mb-3.5">
           Didn’t find what you need?
@@ -75,10 +77,7 @@ export default function ShoppingList() {
                 </h3>
                 <ol>
                   {itemsByCategory.get(category)!.map((item) => (
-                    <ItemInList
-                      item={item}
-                      key={item.itemId}
-                    />
+                    <ItemInList item={item} key={item.itemId} />
                   ))}
                 </ol>
               </div>
@@ -89,7 +88,58 @@ export default function ShoppingList() {
             <p className="w-fit text-xl font-bold">No items</p>
           </div>
         ))}
+
+      <NameForm />
     </div>
+  );
+}
+
+function NameForm() {
+  const storeApi = useStoreContext();
+  const shoppingList = useStore(storeApi, (state) => state.activeList);
+  const { updateListName } = useStore(storeApi, (state) => state.actions);
+  const { register, reset, handleSubmit } = useForm<{ name: string }>();
+
+  const isFormDisabled = !shoppingList || shoppingList.items.length === 0;
+  const formColor = isFormDisabled ? "#C1C1C4" : "";
+
+  const inputClassName = clsx(
+    "bg-white border-2 focus:outline-none rounded-xl px-6 pr-24 py-5 w-full font-sm",
+    { "border-[#F9A109]": !isFormDisabled, "border-[#C1C1C4]": isFormDisabled }
+  );
+
+  const buttonClassName = clsx(
+    "absolute bottom-0 top-0 right-0 px-4 bg-[#F9A109] text-white rounded-xl font-bold placeholder:text-[#BDBDBD]",
+    { "bg-[#F9A109]": !isFormDisabled, "bg-[#C1C1C4]": isFormDisabled }
+  );
+
+  const onSubmit = ({ name }: { name: string }) => {
+    updateListName(name);
+    reset();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="bg-white fixed w-[24rem] bottom-0 right-0 px-10 py-8"
+    >
+      <fieldset disabled={isFormDisabled}>
+        <div className="relative">
+          <label className="sr-only" htmlFor="listName">
+            Name
+          </label>
+          <input
+            type="text"
+            className={inputClassName}
+            placeholder="Enter a name"
+            {...register("name")}
+          />
+          <button className={buttonClassName} type="submit">
+            Save
+          </button>
+        </div>
+      </fieldset>
+    </form>
   );
 }
 
